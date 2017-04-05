c13b1540ae94d8e6eb5d93e4d27f44966f86fdb4
MessiMy <18583932532@163.com>
2017-04-05 17:00:12 +0800

    工程初始化

---
+++ MyNavigationBar/MyNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyNavigationBar
+//
+//  Created by 梅毅 on 2017/4/5.
+//  Copyright © 2017年 my. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyNavigationBar/MyNavigationBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyNavigationBar
+//
+//  Created by 梅毅 on 2017/4/5.
+//  Copyright © 2017年 my. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
